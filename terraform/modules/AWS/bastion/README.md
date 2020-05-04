58cb5b472ba4d468f5f6a7253c29ebe0f1ddfca8
thirupathi-chintu <thirupathiperaboina@gmail.com>
2020-05-04 18:44:03 +0530

    Bastion Envrionment was added

---
+++ terraform/modules/AWS/bastion/README.md
@@ -0,0 +1,111 @@
+AWS Bastion Terraform module
+===========================================
+
+[![Open Source Helpers](https://www.codetriage.com/guimove/terraform-aws-bastion/badges/users.svg)](https://www.codetriage.com/guimove/terraform-aws-bastion)
+
+Terraform module which creates a secure SSH bastion on AWS.
+
+Mainly inspired by [Securely Connect to Linux Instances Running in a Private Amazon VPC](https://aws.amazon.com/blogs/security/securely-connect-to-linux-instances-running-in-a-private-amazon-vpc/)
+
+Features
+--------
+
+This module will create an SSH bastion to securely connect in SSH  to your private instances.
+![Bastion Infrastrucutre](https://raw.githubusercontent.com/Guimove/terraform-aws-bastion/master/_docs/terraformawsbastion.png)
+All SSH  commands are logged on an S3 bucket for security compliance, in the /logs path.
+
+SSH  users are managed by their public key, simply drop the SSH key of the user in  the /public-keys path of the bucket.
+Keys should be named like 'username.pub', this will create the user 'username' on the bastion server.
+
+Then after you'll be able to connect to the server with : 
+
+```
+ssh [-i path_to_the_private_key] username@bastion-dns-name
+```
+
+From this bastion server, you'll able to connect to all instances on the private subnet. 
+
+If there is a missing feature or a bug - [open an issue](https://github.com/Guimove/terraform-aws-bastion/issues/new).
+
+Usage
+-----
+
+```hcl
+module "bastion" {
+  "source" = "terraform-aws-modules/bastion/aws"
+  "bucket_name" = "my_famous_bucket_name"
+  "region" = "eu-west-1"
+  "vpc_id" = "my_vpc_id"
+  "is_lb_private" = "true|false"
+  "bastion_host_key_pair" = "my_key_pair"
+  "hosted_zone_id" = "my.hosted.zone.name."
+  "bastion_record_name" = "bastion.my.hosted.zone.name."
+  "elb_subnets" = [
+    "subnet-id1a",
+    "subnet-id1b"
+  ]
+  "auto_scaling_group_subnets" = [
+    "subnet-id1a",
+    "subnet-id1b"
+  ]
+  tags = {
+    "name" = "my_bastion_name",
+    "description" = "my_bastion_description"
+  }
+}
+```
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| auto_scaling_group_subnets | List of subnet were the Auto Scalling Group will deploy the instances | list | - | yes |
+| allow_ssh_commands | Allows the SSH user to execute one-off commands. Pass 'True' to enable. Warning: These commands are not logged and increase the vulnerability of the system. Use at your own discretion. | string | "" | no |
+| bastion_host_key_pair | Select the key pair to use to launch the bastion host | string | - | yes |
+| bastion_instance_count | Count of bastion instance created on VPC | string | `1` | no |
+| bastion_launch_configuration_name | Bastion Launch configuration Name, will also be used for the ASG | string | `lc` | no |
+| bastion_ami | The AMI that the Bastion Host will use. If not supplied, the latest Amazon2 AMI will be used. | string | `` | no |
+| bastion_record_name | DNS record name to use for the bastion | string | `` | no |
+| bucket_name | Bucket name were the bastion will store the logs | string | - | yes |
+| bucket_force_destroy | On destroy, bucket and all objects should be destroyed when using true | string | false | no |
+| bucket_versioning | Enable bucket versioning or not | string | true | no |
+| cidrs | List of CIDRs than can access to the bastion. Default : 0.0.0.0/0 | list | `<list>` | no |
+| create_dns_record | Choose if you want to create a record name for the bastion (LB). If true 'hosted_zone_id' and 'bastion_record_name' are mandatory | integer | - | yes |
+| elb_subnets | List of subnet were the ELB will be deployed | list | - | yes |
+| extra_user_data_content | Additional scripting to pass to the bastion host. For example, this can include installing postgresql for the `psql` command. | string | `""` | no |
+| hosted_zone_id | Name of the hosted zone were we'll register the bastion DNS name | string | `` | no |
+| is_lb_private | If TRUE the load balancer scheme will be "internal" else "internet-facing" | string | - | yes |
+| log_auto_clean | Enable or not the lifecycle | string | `false` | no |
+| log_expiry_days | Number of days before logs expiration | string | `90` | no |
+| log_glacier_days | Number of days before moving logs to Glacier | string | `60` | no |
+| log_standard_ia_days | Number of days before moving logs to IA Storage | string | `30` | no |
+| private_ssh_port | Set the SSH port to use between the bastion and private instance | string | `22` | no |
+| public_ssh_port | Set the SSH port to use from desktop to the bastion | string | `22` | no |
+| region |  | string | - | yes |
+| tags | A mapping of tags to assign | map | `<map>` | no |
+| vpc_id | VPC id were we'll deploy the bastion | string | - | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| bucket_name |  |
+| bucket_name | The name of the bucket where logs are sent |
+| elb_ip | The ELB DNS Name for the Bastion Host instances |
+| bastion_host_security_group_id | The security group ID of the Bastion Host |
+| private_instances_security_group | The security group ID of the the private instances that allow Bastion SSH ingress |
+
+Known issues
+------------
+
+Tags are not applied to the instances generated by the auto scaling group do to known terraform issue : 
+terraform-providers/terraform-provider-aws#290
+
+Authors
+-------
+
+Module managed by [Guimove](https://github.com/Guimove).
+
+License
+-------
+
+Apache 2 Licensed. See LICENSE for full details.
---
+++ terraform/modules/AWS/bastion/main.tf
@@ -0,0 +1,306 @@
+data "template_file" "user_data" {
+  template = file("${path.module}/user_data.sh")
+
+  vars = {
+    aws_region              = var.region
+    bucket_name             = var.bucket_name
+    extra_user_data_content = var.extra_user_data_content
+    allow_ssh_commands      = var.allow_ssh_commands
+  }
+}
+
+resource "aws_kms_key" "key" {
+  tags = merge(var.tags)
+}
+
+resource "aws_kms_alias" "alias" {
+  name          = "alias/${var.bucket_name}"
+  target_key_id = aws_kms_key.key.arn
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl    = "bucket-owner-full-control"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.key.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+
+  force_destroy = var.bucket_force_destroy
+
+  versioning {
+    enabled = var.bucket_versioning
+  }
+
+  lifecycle_rule {
+    id      = "log"
+    enabled = var.log_auto_clean
+
+    prefix = "logs/"
+
+    tags = {
+      rule      = "log"
+      autoclean = var.log_auto_clean
+    }
+
+    transition {
+      days          = var.log_standard_ia_days
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = var.log_glacier_days
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = var.log_expiry_days
+    }
+  }
+
+  tags = merge(var.tags)
+}
+
+resource "aws_s3_bucket_object" "bucket_public_keys_readme" {
+  bucket  = aws_s3_bucket.bucket.id
+  key     = "public-keys/README.txt"
+  content = "Drop here the ssh public keys of the instances you want to control"
+}
+
+resource "aws_security_group" "bastion_host_security_group" {
+  description = "Enable SSH access to the bastion host from external via SSH port"
+  name        = "${local.name_prefix}-host"
+  vpc_id      = var.vpc_id
+
+  tags = merge(var.tags)
+}
+
+resource "aws_security_group_rule" "ingress_bastion" {
+  description = "Incoming traffic to bastion"
+  type        = "ingress"
+  from_port   = var.public_ssh_port
+  to_port     = var.public_ssh_port
+  protocol    = "TCP"
+  cidr_blocks = concat(data.aws_subnet.subnets.*.cidr_block, var.cidrs)
+
+  security_group_id = aws_security_group.bastion_host_security_group.id
+}
+
+resource "aws_security_group_rule" "egress_bastion" {
+  description = "Outgoing traffic from bastion to instances"
+  type        = "egress"
+  from_port   = "0"
+  to_port     = "65535"
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.bastion_host_security_group.id
+}
+
+resource "aws_security_group" "private_instances_security_group" {
+  description = "Enable SSH access to the Private instances from the bastion via SSH port"
+  name        = "${local.name_prefix}-priv-instances"
+  vpc_id      = var.vpc_id
+
+  tags = merge(var.tags)
+}
+
+resource "aws_security_group_rule" "ingress_instances" {
+  description = "Incoming traffic from bastion"
+  type        = "ingress"
+  from_port   = var.public_ssh_port
+  to_port     = var.public_ssh_port
+  protocol    = "TCP"
+
+  source_security_group_id = aws_security_group.bastion_host_security_group.id
+
+  security_group_id = aws_security_group.private_instances_security_group.id
+}
+
+data "aws_iam_policy_document" "assume_policy_document" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "bastion_host_role" {
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_policy_document.json
+}
+
+data "aws_iam_policy_document" "bastion_host_policy_document" {
+
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+    resources = ["${aws_s3_bucket.bucket.arn}/logs/*"]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = ["${aws_s3_bucket.bucket.arn}/public-keys/*"]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [
+    aws_s3_bucket.bucket.arn]
+
+    condition {
+      test     = "ForAnyValue:StringEquals"
+      values   = ["public-keys/"]
+      variable = "s3:prefix"
+    }
+  }
+
+  statement {
+    actions = [
+
+      "kms:Encrypt",
+      "kms:Decrypt"
+    ]
+    resources = [aws_kms_key.key.arn]
+  }
+
+}
+
+
+resource "aws_route53_record" "bastion_record_name" {
+  name    = var.bastion_record_name
+  zone_id = var.hosted_zone_id
+  type    = "A"
+  count   = var.create_dns_record ? 1 : 0
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.bastion_lb.dns_name
+    zone_id                = aws_lb.bastion_lb.zone_id
+  }
+}
+
+resource "aws_lb" "bastion_lb" {
+  internal = var.is_lb_private
+  name     = "${local.name_prefix}-lb"
+
+  subnets = var.elb_subnets
+
+  load_balancer_type = "network"
+  tags               = merge(var.tags)
+}
+
+resource "aws_lb_target_group" "bastion_lb_target_group" {
+  name        = "${local.name_prefix}-lb-target"
+  port        = var.public_ssh_port
+  protocol    = "TCP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+
+  health_check {
+    port     = "traffic-port"
+    protocol = "TCP"
+  }
+
+  tags = merge(var.tags)
+}
+
+resource "aws_lb_listener" "bastion_lb_listener_22" {
+  default_action {
+    target_group_arn = aws_lb_target_group.bastion_lb_target_group.arn
+    type             = "forward"
+  }
+
+  load_balancer_arn = aws_lb.bastion_lb.arn
+  port              = var.public_ssh_port
+  protocol          = "TCP"
+}
+
+resource "aws_iam_instance_profile" "bastion_host_profile" {
+  role = aws_iam_role.bastion_host_role.name
+  path = "/"
+}
+
+resource "aws_launch_template" "bastion_launch_template" {
+  name_prefix   = local.name_prefix
+  image_id      = var.bastion_ami != "" ? var.bastion_ami : data.aws_ami.amazon-linux-2.id
+  instance_type = "t3.nano"
+  monitoring {
+    enabled = true
+  }
+  network_interfaces {
+    associate_public_ip_address = var.associate_public_ip_address
+    security_groups             = [aws_security_group.bastion_host_security_group.id]
+    delete_on_termination       = true
+  }
+  iam_instance_profile {
+    name = aws_iam_instance_profile.bastion_host_profile.name
+  }
+  key_name = var.bastion_host_key_pair
+
+  user_data = base64encode(data.template_file.user_data.rendered)
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = merge(map("Name", var.bastion_launch_template_name), merge(var.tags))
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+    tags          = merge(map("Name", var.bastion_launch_template_name), merge(var.tags))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "bastion_auto_scaling_group" {
+  name_prefix = "ASG-${local.name_prefix}"
+  launch_template {
+    id      = aws_launch_template.bastion_launch_template.id
+    version = "$Latest"
+  }
+  max_size         = var.bastion_instance_count
+  min_size         = var.bastion_instance_count
+  desired_capacity = var.bastion_instance_count
+
+  vpc_zone_identifier = var.auto_scaling_group_subnets
+
+  default_cooldown          = 180
+  health_check_grace_period = 180
+  health_check_type         = "EC2"
+
+  target_group_arns = [
+    aws_lb_target_group.bastion_lb_target_group.arn,
+  ]
+
+  termination_policies = [
+    "OldestLaunchConfiguration",
+  ]
+
+  tags = concat(
+    list(map("key", "Name", "value", "ASG-${local.name_prefix}", "propagate_at_launch", true)),
+    local.tags_asg_format
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
